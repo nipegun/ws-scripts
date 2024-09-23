48135ca4f9c92eae58a5ec5d0d0acc21766cdc88
NiPeGun <nipegun@gmail.com>
2024-09-23 13:40:18 +0200

    Update ControladorDeDominio-Instalar.ps1

--- ControladorDeDominio-Instalar.ps1
+++ ControladorDeDominio-Instalar.ps1
@@ -1,1 +1,14 @@
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+param (
+  [Parameter(Mandatory=$true)]
+  [string]$vDominio
+  [string]$vExtens
+
+  [int]$vIP
+)
+
+Write-Host "Se va a proceder a instalar el controplador de dominio para el dominio $vDominio.$vExtens"
