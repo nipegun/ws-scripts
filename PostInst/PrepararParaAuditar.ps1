438f09e5af0b77e496e9ff9a64d6237158dc869f
NiPeGun <nipegun@gmail.com>
2024-10-16 17:41:55 +0200

    Create PrepararParaAuditar.ps1

---
+++ PostInst/PrepararParaAuditar.ps1
@@ -0,0 +1,36 @@
+# secpol.msc
+  # Configuración de seguridad
+    # Configuración de políticas de auditoría avanzada
+      # Directivas de auditoría del sistema
+        # Inicio de sesión de cuentas (Categoría: "Inicio de sesión de la cuenta")
+          AuditPol /set /subcategory:"Validación de credenciales" /success:enable /failure:enable
+
+        # Administración de cuentas
+        # Seguimiento detallado
+        # Acceso DS
+        # Inicio y cierre de sesión
+        # Acceso a objetos
+        # Cambio de directivas
+        # Uso de privilegios
+        # Sistema
+        # Auditoría de acceso a objetos globalirectivas locales
+# Para comprobar las categorías configuradas
+AuditPol /get /category:*
+
+  
+$policies = @(
+    @{ Subcategory = "Logon"; Success = "enable"; Failure = "enable" },
+    @{ Subcategory = "Account Lockout"; Success = "enable"; Failure = "enable" },
+    @{ Subcategory = "Process Creation"; Success = "enable"; Failure = "enable" }
+)
+
+# Aplicar las políticas de auditoría
+foreach ($policy in $policies) {
+    AuditPol /set /subcategory:$($policy.Subcategory) /success:$($policy.Success) /failure:$($policy.Failure)
+}
+
+# Visualizar cambioa
+  Write-Host '\n  La categoría "Inicio de sesión de la cuenta" ha quedado de la siguiente manera: '
+  AuditPol /get /category:"Inicio de sesión de la cuenta"
+  Write-Host ''
+  
