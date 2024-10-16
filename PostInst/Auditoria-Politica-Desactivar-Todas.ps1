2a8245f0360c716079a7ddc1d3f367377919019a
NiPeGun <nipegun@gmail.com>
2024-10-16 18:01:54 +0200

    Update Auditoria-Politica-Desactivar-Todas.ps1

--- PostInst/Auditoria-Politica-Desactivar-Todas.ps1
+++ PostInst/Auditoria-Politica-Desactivar-Todas.ps1
@@ -8,3 +8,20 @@ $policies = @(
 foreach ($policy in $policies) {
     AuditPol /set /subcategory:$($policy.Subcategory) /success:$($policy.Success) /failure:$($policy.Failure)
 }
+
+
+
+
+
+
+# Obtener todas las subcategorías de auditoría
+$subcategories = AuditPol /get /subcategory:* | Select-String " - " | ForEach-Object {
+    $_ -replace " - .*", "" -replace "\s+$", ""
+}
+
+# Deshabilitar la auditoría de éxito y fracaso para todas las subcategorías
+foreach ($subcategory in $subcategories) {
+    AuditPol /set /subcategory:"$subcategory" /success:disable /failure:disable
+}
+
+Write-Output "Todas las categorías de auditoría se han configurado como 'sin configurar'."
