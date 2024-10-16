f844fdd5458076b2453d9987b293896e0b1576aa
NiPeGun <nipegun@gmail.com>
2024-10-16 21:10:16 +0200

    Update and rename PrepararParaAuditar1.ps1 to PrepararParaAuditar2.ps1

--- PostInst/PrepararParaAuditar2.ps1
+++ PostInst/PrepararParaAuditar2.ps1
@@ -9,11 +9,17 @@
     # Configuración de políticas de auditoría avanzada
       # Directivas de auditoría del sistema
         # Inicio de sesión de cuentas (Categoría: "Inicio de sesión de la cuenta")
+          Write-Host ''
+          Write-Host '  Modificando políticas de Inicio de sesión de cuentas: '
+          Write-Host ''
           AuditPol /set /subcategory:"Validación de credenciales"                   /success:enable  /failure:enable
           AuditPol /set /subcategory:"Servicio de autenticación Kerberos"           /success:disable /failure:disable
           AuditPol /set /subcategory:"Operaciones de vales de servicio Kerberos"    /success:disable /failure:disable
           AuditPol /set /subcategory:"Otros eventos de inicio de sesión de cuentas" /success:enable  /failure:enable
         # Administración de cuentas
+          Write-Host ''
+          Write-Host '  Modificando políticas de Administración de cuentas: '
+          Write-Host ''
           AuditPol /set /subcategory:"Administración de grupos de aplicaciones"   /success:enable /failure:enable
           AuditPol /set /subcategory:"Administración de cuentas de equipo"        /success:enable /failure:enable
           AuditPol /set /subcategory:"Administración de grupos de distribución"   /success:enable /failure:enable
@@ -21,6 +27,9 @@
           AuditPol /set /subcategory:"Administración de grupos de seguridad"      /success:enable /failure:enable
           AuditPol /set /subcategory:"Administración de cuentas de usuario"       /success:enable /failure:enable
         # Seguimiento detallado
+          Write-Host ''
+          Write-Host '  Modificando políticas de Seguimiento detallado: '
+          Write-Host ''
           AuditPol /set /subcategory:"Actividad DPAPI"                       /success:disable /failure:disable
           AuditPol /set /subcategory:"Eventos Plug and Play"                 /success:enable  /failure:disable
           AuditPol /set /subcategory:"Creación del proceso"                  /success:enable  /failure:enable
@@ -28,11 +37,17 @@
           AuditPol /set /subcategory:"Eventos de RPC"                        /success:enable  /failure:enable
           AuditPol /set /subcategory:"Eventos de ajuste de derecho de token" /success:enable  /failure:disable
         # Acceso DS
+          Write-Host ''
+          Write-Host '  Modificando políticas de Acceso DS: '
+          Write-Host ''
           AuditPol /set /subcategory:"Replicación de servicio de directorio detallada" /success:disable /failure:disable
           AuditPol /set /subcategory:"Acceso del servicio de directorio"               /success:disable /failure:disable
           AuditPol /set /subcategory:"Cambios de servicio de directorio"               /success:enable  /failure:enable
           AuditPol /set /subcategory:"Replicación de servicio de directorio"           /success:disable /failure:disable
         # Inicio y cierre de sesión
+          Write-Host ''
+          Write-Host '  Modificando políticas de Inicio y cierre de sesión: '
+          Write-Host ''
           AuditPol /set /subcategory:"Bloqueo de cuenta"                          /success:enable  /failure:disable
           AuditPol /set /subcategory:"Notificaciones de usuario o dispositivo"    /success:disable /failure:disable
           AuditPol /set /subcategory:"Pertenencia a grupos"                       /success:enable  /failure:disable
@@ -45,6 +60,9 @@
           AuditPol /set /subcategory:"Otros eventos de inicio y cierre de sesión" /success:enable  /failure:enable
           AuditPol /set /subcategory:"Inicio de sesión especial"                  /success:enable  /failure:enable
         # Acceso a objetos
+          Write-Host ''
+          Write-Host '  Modificando políticas de Acceso a objetos: '
+          Write-Host ''
           AuditPol /set /subcategory:"Aplicación generada"                              /success:enable  /failure:enable
           AuditPol /set /subcategory:"Servicios de certificación"                       /success:enable  /failure:enable
           AuditPol /set /subcategory:"Recurso compartido de archivos detallado"         /success:enable  /failure:disable
@@ -60,6 +78,9 @@
           AuditPol /set /subcategory:"SAM"                                              /success:enable  /failure:disable
           AuditPol /set /subcategory:"Almacenamiento provisional de directiva central"  /success:disable /failure:disable
         # Cambio de directivas
+          Write-Host ''
+          Write-Host '  Modificando políticas de Cambio de directivas: '
+          Write-Host ''
           AuditPol /set /subcategory:"Cambio de directiva de auditoría"                 /success:enable  /failure:enable
           AuditPol /set /subcategory:"Cambio de directiva de autenticación"             /success:enable  /failure:enable
           AuditPol /set /subcategory:"Cambio de directiva de autorización"              /success:enable  /failure:enable
@@ -67,18 +88,21 @@
           AuditPol /set /subcategory:"Cambio de directiva de nivel de reglas de MPSSVC" /success:disable /failure:disable
           AuditPol /set /subcategory:"Otros eventos de cambios de directiva"            /success:disable /failure:disable
         # Uso de privilegios
+          Write-Host ''
+          Write-Host '  Modificando políticas de Uso de privilegios: '
+          Write-Host ''
           AuditPol /set /subcategory:"Uso de privilegio no confidencial"  /success:disable /failure:disable
           AuditPol /set /subcategory:"Otros eventos de uso de privilegio" /success:disable /failure:disable
           AuditPol /set /subcategory:"Uso de privilegio confidencial"     /success:enable  /failure:enable
           # Sistema
+          Write-Host ''
+          Write-Host '  Modificando políticas de Sistema: '
+          Write-Host ''
           AuditPol /set /subcategory:"Controlador IPSec"                  /success:enable  /failure:disable
           AuditPol /set /subcategory:"Otros eventos de sistema"           /success:disable /failure:enable
           AuditPol /set /subcategory:"Cambio de estado de seguridad"      /success:enable  /failure:enable
           AuditPol /set /subcategory:"Extensión del sistema de seguridad" /success:enable  /failure:enable
           AuditPol /set /subcategory:"Integridad del sistema"             /success:enable  /failure:enable
-        # Auditoría de acceso a objetos global
-          AuditPol /set /subcategory:"Sistema de archivos" /success:enable  /failure:enable
-          AuditPol /set /subcategory:"Registro"            /success:enable  /failure:enable
 
 # Visualizar cambioa
   Write-Host ''
