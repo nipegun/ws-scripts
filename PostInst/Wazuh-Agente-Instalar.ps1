0a24aa79c51173a39ac4a5eb2723569c90ebab0a
NiPeGun <nipegun@gmail.com>
2025-10-04 14:38:10 +0200

    Create Wazuh-Agente-Instalar.ps1

---
+++ PostInst/Wazuh-Agente-Instalar.ps1
@@ -0,0 +1,11 @@
+#!/usr/bin/env pwsh
+
+$vVersWazuh     = '4.x'
+$vArchivoMSI    = 'wazuh-agent_4.13.1-1.msi'
+$vWazuhServerIP = '192.168.1.147'
+
+# Descargar el archivo
+Invoke-WebRequest -Uri "https://packages.wazuh.com/$vVersWazuh/windows/$vArchivoMSI" -OutFile "$env:TEMP\$vArchivoMSI"
+
+# Ejecutar el instalador
+msiexec /i "$env:TEMP\$vArchivoMSI" /q WAZUH_MANAGER=$vWazuhServerIP
